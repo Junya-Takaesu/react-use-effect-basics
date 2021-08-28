0027a301cde8ef7ee1dd989cd20df9d9edb19f15
junya takaesu <junya.takaesu@gmail.com>
2021-08-28 14:54:45 +0900

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import UseEffectBasics from "./Components/UseEffectBasics";
+
+function App() {
+  return (
+    <div className="container">
+      <UseEffectBasics />
+    </div>
+  );
+}
+
+export default App;
